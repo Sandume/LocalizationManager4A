2773e8255f0ec8828f593dd8155fcc6b379cdbde
Léo <leo413568@gmail.com>
2024-10-29 16:03:28 +0100

    add import export base

--- LocalizationManager/LocalizationManagerTool/MainWindow.xaml.cs
+++ LocalizationManager/LocalizationManagerTool/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
@@ -14,11 +15,24 @@ namespace LocalizationManagerTool
     /// 
     public partial class MainWindow : Window
     {
+
+        public class Translation
+        {
+            public string Id { get; set; }
+            public string En { get; set; }
+            public string Fr { get; set; }
+            public string Es { get; set; }
+            public string Ja { get; set; }
+        }
+
         public List<string> Columns = new List<string>();
+        public ObservableCollection<Translation> Translations { get; set; }
 
         public MainWindow()
         {
             InitializeComponent();
+            Translations = new ObservableCollection<Translation>();
+            dataGrid.ItemsSource = Translations; // Lier le DataGrid à la liste de traductions
             Columns.Add("id");
             Columns.Add("en");
             Columns.Add("fr");
@@ -35,6 +49,7 @@ namespace LocalizationManagerTool
             }
         }
 
+        #region buttons
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
@@ -64,35 +79,126 @@ namespace LocalizationManagerTool
 
         }
 
-        private void ImportCsv(string filePath)
+        private void ExportCsvButton_Click(object sender, RoutedEventArgs e)
         {
-            var lines = File.ReadAllLines(filePath);
-            foreach (var line in lines)
+            var saveFileDialog = new Microsoft.Win32.SaveFileDialog
             {
-                var values = line.Split(',');
-                // Crée un objet avec ces valeurs et ajoute-le à la source de données
+                Filter = "CSV files (*.csv)|*.csv",
+                FileName = "translations.csv"
+            };
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                ExportCsv(saveFileDialog.FileName);
             }
         }
 
+        private void ExportJsonButton_Click(object sender, RoutedEventArgs e)
+        {
+            var saveFileDialog = new Microsoft.Win32.SaveFileDialog
+            {
+                Filter = "JSON files (*.json)|*.json",
+                FileName = "translations.json"
+            };
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                ExportJson(saveFileDialog.FileName);
+            }
+        }
+
+        private void ExportXmlButton_Click(object sender, RoutedEventArgs e)
+        {
+            var saveFileDialog = new Microsoft.Win32.SaveFileDialog
+            {
+                Filter = "JSON files (*.xml)|*.xml",
+                FileName = "translations.xml"
+            };
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                ExportXml(saveFileDialog.FileName);
+            }
+        }
+
+        private void ExportClassButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ExportCppHeaderButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExportCppSourceButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region CSV
+        private void ImportCsv(string filePath)
+        {
+            try
+            {
+                var lines = File.ReadAllLines(filePath);
+                Translations.Clear(); 
+                foreach (var line in lines.Skip(1))
+                {
+                    var values = line.Split(',');
+                    if (values.Length == 5)
+                    {
+                        Translations.Add(new Translation
+                        {
+                            Id = values[0],
+                            En = values[1],
+                            Fr = values[2],
+                            Es = values[3],
+                            Ja = values[4]
+                        });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erreur lors de l'importation : {ex.Message}");
+            }
+        }
+
+        private void ExportCsv(string filePath)
+        {
+            var lines = new List<string> { "id,en,fr,es,ja" };
+            lines.AddRange(Translations.Select(t => $"{t.Id},{t.En},{t.Fr},{t.Es},{t.Ja}"));
+            File.WriteAllLines(filePath, lines);
+        }
+        #endregion
+
+        #region json
         private void ImportJson(string filePath)
         {
             var jsonData = File.ReadAllText(filePath);
             var translations = JsonConvert.DeserializeObject<List<Translation>>(jsonData);
-            // Remplis ta DataGrid avec ces traductions
+            Translations.Clear();
+            foreach (var translation in translations)
+            {
+                Translations.Add(translation);
+            }
         }
 
-        public class Translation
+        private void ExportJson(string filePath)
         {
-            public string Id { get; set; }
-            public string En { get; set; }
-            public string Fr { get; set; }
-            public string Es { get; set; }
-            public string Ja { get; set; }
+            var jsonData = JsonConvert.SerializeObject(Translations, Formatting.Indented);
+            File.WriteAllText(filePath, jsonData);
         }
+        #endregion
 
+        #region Xmls
         private void ImportXml(string filePath)
         {
             XDocument xDoc = XDocument.Load(filePath);
+            Translations.Clear();
             foreach (var element in xDoc.Descendants("translation"))
             {
                 var translation = new Translation
@@ -103,8 +209,83 @@ namespace LocalizationManagerTool
                     Es = element.Element("es")?.Value,
                     Ja = element.Element("ja")?.Value
                 };
-                // Ajoute ce modèle à la source de données
+                Translations.Add(translation);
             }
         }
+
+        private void ExportXml(string filePath)
+        {
+            var xDoc = new XDocument(new XElement("translations",
+                Translations.Select(t => new XElement("translation",
+                    new XElement("id", t.Id),
+                    new XElement("en", t.En),
+                    new XElement("fr", t.Fr),
+                    new XElement("es", t.Es),
+                    new XElement("ja", t.Ja)
+                ))
+            ));
+            xDoc.Save(filePath);
+        }
+
+        #endregion
+
+        #region ExportClass
+        private void ExportClass(string filePath)
+        {
+            var classContent = @"
+using System.Collections.Generic;
+
+public class Translations
+{
+    public static Dictionary<string, string> TranslationData = new Dictionary<string, string>
+    {
+";
+            foreach (var translation in Translations)
+            {
+                classContent += $"        {{ \"{translation.Id}\", \"{translation.En}\" }},\n";
+            }
+            classContent += @"
+    };
+}";
+            File.WriteAllText(filePath, classContent);
+        }
+        #endregion
+
+        #region exportCPP
+        private void ExportCppHeader(string filePath)
+        {
+            var headerContent = @"
+#ifndef TRANSLATIONS_H
+#define TRANSLATIONS_H
+
+#include <map>
+#include <string>
+
+class Translations
+{
+public:
+    static std::map<std::string, std::string> TranslationData;
+};
+
+#endif // TRANSLATIONS_H";
+            File.WriteAllText(Path.ChangeExtension(filePath, ".h"), headerContent);
+        }
+
+        private void ExportCppSource(string filePath)
+        {
+            var sourceContent = @"
+#include ""Translations.h""
+
+std::map<std::string, std::string> Translations::TranslationData = {
+";
+            foreach (var translation in Translations)
+            {
+                sourceContent += $"    {{ \"{translation.Id}\", \"{translation.En}\" }},\n";
+            }
+            sourceContent += @"};
+";
+            File.WriteAllText(Path.ChangeExtension(filePath, ".cpp"), sourceContent);
+        }
     }
+    #endregion
 }
